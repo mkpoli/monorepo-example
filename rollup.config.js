5c4de9e0a41d337b4e964dd656e4712b2e6b45d3
mkpoli <snowwolf1001@gmail.com>
2022-02-13 06:53:13 +0900

    Add Rollup

---
+++ rollup.config.js
@@ -0,0 +1,36 @@
+import resolve from '@rollup/plugin-node-resolve'
+import { terser } from 'rollup-plugin-terser'
+import ts from 'rollup-plugin-ts'
+
+import path from 'path'
+
+const { LERNA_PACKAGE_NAME } = process.env
+const PACKAGE_ROOT_PATH = process.cwd()
+export default [
+  {
+    input: path.join(PACKAGE_ROOT_PATH, 'src', 'index.ts'),
+    output: [
+      {
+        file: `dist/index.cjs.js`,
+        format: 'cjs',
+        sourcemap: true,
+      },
+      {
+        file: `dist/index.esm.js`,
+        format: 'esm',
+        sourcemap: true,
+      },
+      {
+        name: LERNA_PACKAGE_NAME,
+        file: `dist/index.umd.js`,
+        sourcemap: true,
+        format: 'umd',
+      },
+    ],
+    plugins: [
+      ts(),
+      resolve(),
+      terser()
+    ],
+  },
+]
