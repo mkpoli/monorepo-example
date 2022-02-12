9f4d3afa874fcc83788d4bba1d5e6aca8d90386c
mkpoli <snowwolf1001@gmail.com>
2022-02-13 00:16:42 +0900

    Use minus package in app package

--- packages/app/src/index.js
+++ packages/app/src/index.js
@@ -1,3 +1,5 @@
 import { plus } from '@mkpoli-arithmetic/plus'
+import { minus } from '@mkpoli-arithmetic/minus'
 
-console.log(plus(1, 2))
+console.log('1 + 2 =', plus(1, 2))
+console.log('3 - 2 =', minus(3, 2))
